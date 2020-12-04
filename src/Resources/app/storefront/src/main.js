cd5b2cfe8105c1681760d6edc1cd45716ca1b0ef
tinect <s.koenig@tinect.de>
2020-12-04 10:31:54 +0100

    remove superfluous code

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,14 +1,5 @@
-import Debouncer from 'src/helper/debouncer.helper';
 import 'lazysizes';
 import 'lazysizes/plugins/native-loading/ls.native-loading';
 import 'lazysizes/plugins/attrchange/ls.attrchange';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 import 'lazysizes/plugins/object-fit/ls.object-fit';
-
-document.addEventListener('lazyloaded', Debouncer.debounce(function(event){
-    if(event.target.classList.contains('tns-complete')) {
-        const resizeEvent = window.document.createEvent('UIEvents');
-        resizeEvent.initUIEvent('resize', true, false, window, 0);
-        window.dispatchEvent(resizeEvent);
-    }
-}, 400));
