6e2603db3aecae32cf82046efa8f0f3296f62710
pumpi <webmaster@pumpi-online.de>
2022-02-08 11:40:41 +0100

    feat: encode shopware media path without prefixes

--- src/Service/ThumbnailUrlTemplate.php
+++ src/Service/ThumbnailUrlTemplate.php
@@ -21,6 +21,14 @@ class ThumbnailUrlTemplate implements ThumbnailUrlTemplateInterface
 
     public function getUrl(string $mediaUrl, string $mediaPath, string $width, string $height = ''): string
     {
+        $segments = \explode('/', $mediaPath);
+
+        foreach ($segments as $index => $segment) {
+            $segments[$index] = \rawurlencode($segment);
+        }
+
+        $mediaPath = implode('/', $segments);
+
         return str_replace(
             ['{mediaUrl}', '{mediaPath}', '{width}', '{height}'],
             [$mediaUrl, $mediaPath, $width, ''],
