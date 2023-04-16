c4d76defa8feb0bfaa788028fee86426fa31c128
tinect <s.koenig@tinect.de>
2023-04-16 21:54:34 +0200

    feat: use php parser to overwrite ThumbnailService
    (cherry picked from commit d6c321b6042ebfede134d095e192298324114118)

--- src/FroshPlatformThumbnailProcessor.php
+++ src/FroshPlatformThumbnailProcessor.php
@@ -2,8 +2,21 @@
 
 namespace Frosh\ThumbnailProcessor;
 
+use Frosh\ThumbnailProcessor\DependencyInjection\ThumbnailServiceGeneratorPass;
 use Shopware\Core\Framework\Plugin;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class FroshPlatformThumbnailProcessor extends Plugin
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new ThumbnailServiceGeneratorPass());
+
+        parent::build($container);
+    }
+
+    public function executeComposerCommands(): bool
+    {
+        return true;
+    }
 }
