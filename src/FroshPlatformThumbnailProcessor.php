57cb2c899b0f2090b70e8172ba123a253ea8793f
tinect <s.koenig@tinect.de>
2023-06-02 23:25:13 +0200

    feat: replace FileSaver to support media renaming

--- src/FroshPlatformThumbnailProcessor.php
+++ src/FroshPlatformThumbnailProcessor.php
@@ -2,7 +2,9 @@
 
 namespace Frosh\ThumbnailProcessor;
 
-use Frosh\ThumbnailProcessor\DependencyInjection\ThumbnailServiceGeneratorPass;
+use Frosh\ThumbnailProcessor\DependencyInjection\GeneratorCompilerPass;
+use Shopware\Core\Content\Media\File\FileSaver;
+use Shopware\Core\Content\Media\Thumbnail\ThumbnailService;
 use Shopware\Core\Framework\Plugin;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -10,7 +12,8 @@ class FroshPlatformThumbnailProcessor extends Plugin
 {
     public function build(ContainerBuilder $container): void
     {
-        $container->addCompilerPass(new ThumbnailServiceGeneratorPass());
+        $container->addCompilerPass(new GeneratorCompilerPass(ThumbnailService::class));
+        $container->addCompilerPass(new GeneratorCompilerPass(FileSaver::class));
 
         parent::build($container);
     }
