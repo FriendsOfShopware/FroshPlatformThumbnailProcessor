484a14337360053c5379092e25268e2b0d6fdcf0
Alex <git@jinnoflife.com>
2023-07-19 22:29:12 +0200

    feat: Adjust administration handling for test button and add some more context (#100)

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -1,3 +1,4 @@
 import './module/frosh-thumbnail-processor/frosh-thumbnail-processor-info-texts';
 import './module/frosh-thumbnail-processor/test-button'
+
 import './service/thumbnailProcessorTestService';
