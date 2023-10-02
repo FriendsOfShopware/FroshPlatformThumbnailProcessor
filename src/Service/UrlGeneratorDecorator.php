bd0b259bc3d87bd7133e14bf62095f98aee4be53
tinect <s.koenig@tinect.de>
2023-10-03 00:39:38 +0200

    feat: update phpstan rules

--- src/Service/UrlGeneratorDecorator.php
+++ src/Service/UrlGeneratorDecorator.php
@@ -11,6 +11,7 @@ use Shopware\Core\Content\Media\Pathname\UrlGeneratorInterface;
 /**
  * We have to extend here, otherwise this would be thrown:
  * Shopware\Core\Content\Media\Core\Strategy\BCStrategy::__construct(): Argument #3 ($generator) must be of type Shopware\Core\Content\Media\Pathname\UrlGenerator, Frosh\ThumbnailProcessor\Service\UrlGeneratorDecorator given
+ * TODO: check PR https://github.com/shopware/platform/pull/3337
  */
 class UrlGeneratorDecorator extends UrlGenerator
 {
