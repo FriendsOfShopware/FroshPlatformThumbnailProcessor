e29272627629816d86f191ca2c65a7889d4ebe6b
Daniel Melchior <daniel.melchior@interweber.de>
2024-05-28 11:16:10 +0200

    feature: allow passing media timestamp to image service

--- src/Service/ThumbnailUrlTemplateInterface.php
+++ src/Service/ThumbnailUrlTemplateInterface.php
@@ -4,5 +4,5 @@ namespace Frosh\ThumbnailProcessor\Service;
 
 interface ThumbnailUrlTemplateInterface
 {
-    public function getUrl(string $mediaUrl, string $mediaPath, string $width): string;
+    public function getUrl(string $mediaUrl, string $mediaPath, string $width, ?\DateTimeInterface $mediaUpdatedAt): string;
 }
--- tests/unit/Service/ThumbnailUrlTemplateTest.php
+++ tests/unit/Service/ThumbnailUrlTemplateTest.php
@@ -9,54 +9,54 @@ use Shopware\Core\Framework\Uuid\Uuid;
 
 class ThumbnailUrlTemplateTest extends TestCase
 {
-    /**
+	/**
      * @dataProvider getSalesChannelIds
      */
-    public function testGetUrl(?string $salesChannelId, string $mediaUrl, string $mediaPath, string $width): void
+    public function testGetUrl(?string $salesChannelId, string $mediaUrl, string $mediaPath, string $width, ?\DateTimeInterface $date): void
     {
         $configReader = $this->createMock(ConfigReader::class);
         $configReader->expects(static::once())
-            ->method('getConfig')->willReturn('{mediaUrl}/{mediaPath}?width={width}&uff');
+            ->method('getConfig')->willReturn('{mediaUrl}/{mediaPath}?width={width}&updatedAt={mediaUpdatedAt}&uff');
 
         $class = new ThumbnailUrlTemplate($configReader);
 
-        $url = $class->getUrl($mediaUrl, $mediaPath, $width);
+        $url = $class->getUrl($mediaUrl, $mediaPath, $width, $date);
 
-        static::assertSame(\sprintf('%s/%s?width=%s&uff', $mediaUrl, $mediaPath, $width), $url);
+        static::assertSame(\sprintf('%s/%s?width=%s&updatedAt=%s&uff', $mediaUrl, $mediaPath, $width, $date?->getTimestamp() ?? 'null'), $url);
     }
 
     /**
      * @dataProvider getSalesChannelIds
      */
-    public function testGetUrlWithoutSetConfig(?string $salesChannelId, string $mediaUrl, string $mediaPath, string $width): void
+    public function testGetUrlWithoutSetConfig(?string $salesChannelId, string $mediaUrl, string $mediaPath, string $width, ?\DateTimeInterface $date): void
     {
         $configReader = $this->createMock(ConfigReader::class);
         $configReader->expects(static::once())
-            ->method('getConfig')->willReturn('{mediaUrl}/{mediaPath}?width={width}');
+            ->method('getConfig')->willReturn('{mediaUrl}/{mediaPath}?width={width}&updatedAt={mediaUpdatedAt}');
 
         $class = new ThumbnailUrlTemplate($configReader);
 
-        $url = $class->getUrl($mediaUrl, $mediaPath, $width);
+        $url = $class->getUrl($mediaUrl, $mediaPath, $width, $date);
 
-        static::assertSame(\sprintf('%s/%s?width=%s', $mediaUrl, $mediaPath, $width), $url);
+        static::assertSame(\sprintf('%s/%s?width=%s&updatedAt=%s', $mediaUrl, $mediaPath, $width, $date?->getTimestamp() ?? 'null'), $url);
     }
 
     /**
      * @dataProvider getSalesChannelIds
      */
-    public function testGetUrlGetPatternOnce(?string $salesChannelId, string $mediaUrl, string $mediaPath, string $width): void
+    public function testGetUrlGetPatternOnce(?string $salesChannelId, string $mediaUrl, string $mediaPath, string $width, ?\DateTimeInterface $date): void
     {
         $configReader = $this->createMock(ConfigReader::class);
         $configReader->expects(static::once())
-            ->method('getConfig')->willReturn('{mediaUrl}/{mediaPath}?width={width}');
+            ->method('getConfig')->willReturn('{mediaUrl}/{mediaPath}?width={width}&updatedAt={mediaUpdatedAt}');
 
         $class = new ThumbnailUrlTemplate($configReader);
 
-        $class->getUrl($mediaUrl, $mediaPath, $width);
+        $class->getUrl($mediaUrl, $mediaPath, $width, $date);
 
-        $url = $class->getUrl($mediaUrl, $mediaPath, $width);
+        $url = $class->getUrl($mediaUrl, $mediaPath, $width, $date);
 
-        static::assertSame(\sprintf('%s/%s?width=%s', $mediaUrl, $mediaPath, $width), $url);
+        static::assertSame(\sprintf('%s/%s?width=%s&updatedAt=%s', $mediaUrl, $mediaPath, $width, $date?->getTimestamp() ?? 'null'), $url);
     }
 
     /**
@@ -64,10 +64,11 @@ class ThumbnailUrlTemplateTest extends TestCase
      */
     public static function getSalesChannelIds(): iterable
     {
-        yield [null, 'https://www.anywebpage.test', 'media/78/a1/myimage.jpg', '200'];
-        yield [Uuid::randomHex(), 'https://www.anyotherwebpage.test', 'media/aa/a1/myimage.jpg', '300'];
-        yield [Uuid::randomHex(), 'https://www.anyother2webpage.test', 'media/aa/bb/myimage.jpg', '700'];
-        yield [Uuid::randomHex(), 'https://www.anyother3webpage.test', 'media/aa/cc/myimage.jpg', '900'];
-        yield [Uuid::randomHex(), 'https://www.anyother4webpage.test', 'media/aa/dd/myimage.jpg', '1000'];
+        yield [null, 'https://www.anywebpage.test', 'media/78/a1/myimage.jpg', '200', null];
+        yield [null, 'https://www.anyotherwebpage.test', 'media/78/a1/myimage.jpg', '200', new \DateTimeImmutable()];
+        yield [Uuid::randomHex(), 'https://www.anyother2webpage.test', 'media/aa/a1/myimage.jpg', '300', new \DateTimeImmutable()];
+        yield [Uuid::randomHex(), 'https://www.anyother3webpage.test', 'media/aa/bb/myimage.jpg', '700', null];
+        yield [Uuid::randomHex(), 'https://www.anyother4webpage.test', 'media/aa/cc/myimage.jpg', '900', new \DateTimeImmutable()];
+        yield [Uuid::randomHex(), 'https://www.anyother5webpage.test', 'media/aa/dd/myimage.jpg', '1000', null];
     }
 }
